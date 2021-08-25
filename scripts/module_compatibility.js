86ac5f3dbddc2163e1db40d6f3e2b32240f3b6ff
Robert McIntosh <rmcintosh@remabledesigns.com>
2021-08-25 15:01:40 -0400

    lib-themer updates

--- scripts/module_compatibility.js
+++ scripts/module_compatibility.js
@@ -1,8 +1,31 @@
 Hooks.on("ready", function() {
 
-    const hasTidySheet = game.modules.get('tidy5e-sheet')?.active;
-    if (hasTidySheet) {
+    if (game.modules.get('tidy5e-sheet')?.active ?? false) {
         game.settings.set("tidy5e-sheet", "colorScheme", "dark");
+    }
+
+    if (game.modules.get('lib-themer')?.active ?? false) {
+        game.modules.get('lib-themer').api.registerTheme('twiligh-ui', {
+            "name": "twilight-ui",
+            "title": "Twilight UI",
+            
+            "--twilight-ui-preset": {
+                "name": "Twight UI Preset",
+                "type": "preset",
+                "default": {
+                    "lib-themer": {
+                        "--palette-app-background": { "default" : "#0d0c0a" },
+                        "--palette-dialog-background": { "default" : "#191716" },
+                        "--palette-primary": { "default" : "#252423" },
+                        "--palette-secondary": { "default" : "#f56d12" },
+                        "--palette-tertiary": { "default" : "#21122F" },
+                        "--palette-success": { "default" : "#418824" },
+                        "--palette-danger": { "default" : "#cc2d2d" },
+                        "--palette-info": { "default" : "#596cf3" }
+                    },
+                },
+            }
+        })
     }
 
 });
